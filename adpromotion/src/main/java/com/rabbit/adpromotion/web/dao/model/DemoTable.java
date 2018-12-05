0a3290009698abf0c4eef1738be4acb3b0fe6050
janus-shieh <42328979+janus-shieh@users.noreply.github.com>
2018-12-05 14:45:14 +0800

    上传测试

--- adpromotion/src/main/java/com/rabbit/adpromotion/web/dao/model/DemoTable.java
+++ adpromotion/src/main/java/com/rabbit/adpromotion/web/dao/model/DemoTable.java
@@ -9,8 +9,6 @@ public class DemoTable implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    
-
     private Integer id;
     private String demoName;
     private Integer demoAge;
